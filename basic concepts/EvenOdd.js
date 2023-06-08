9a88afad29ea9c77bbe7112b88a1efb45e156eed
MariaKousar <69350681+MariaKousar@users.noreply.github.com>
2023-06-08 17:28:22 -0300

    Update EvenOdd.js

--- basic concepts/EvenOdd.js
+++ basic concepts/EvenOdd.js
@@ -2,14 +2,7 @@
 
 const num = 9;
 
-// if(num % 2 == 0){
-//     console.log("The number "+ num + " is even")
-// }
-// else{
-//     console.log("The number is odd")
-// }
-
-// OR turnery operator
+// turnery operator
 
 const result = (num % 2 == 0) ? "even" : "odd";
 console.log(`The number is ${result}`);
